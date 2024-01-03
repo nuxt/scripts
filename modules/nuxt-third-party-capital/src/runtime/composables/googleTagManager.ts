8904ae673850ce30a815eb176f6ee838d29b73d0
Thorsten Kober <tkober@gmail.com>
2024-01-02 20:17:38 -0500

    Use Third-Party-Capital for Google Tag Manager integration (#34)

--- modules/nuxt-third-party-capital/src/runtime/composables/googleTagManager.ts
+++ modules/nuxt-third-party-capital/src/runtime/composables/googleTagManager.ts
@@ -1,42 +1,19 @@
-import { withQuery } from 'ufo'
-import { useServerHead } from '@unhead/vue'
+import { GoogleTagManager } from 'third-party-capital'
 import type { GoogleTagManagerApi, GoogleTagManagerOptions } from 'third-party-capital'
-import { validateRequiredOptions } from '../util'
-
-import type { ThirdPartyScriptOptions } from '../types'
-import { useScript } from '#imports'
-
+import { convertThirdPartyCapital, validateRequiredOptions } from '../util'
+import type { ThirdPartyScriptApi, ThirdPartyScriptOptions } from '../types'
 declare global {
   interface Window extends GoogleTagManagerApi { }
 }
 
-export function useGoogleTagManager(options: ThirdPartyScriptOptions<GoogleTagManagerOptions, GoogleTagManagerApi> = {}) {
-  const key = 'google-tag-manager'
-  validateRequiredOptions(key, options, ['id'])
+export function useGoogleTagManager(options: ThirdPartyScriptOptions<GoogleTagManagerOptions, GoogleTagManagerApi> = {}): ThirdPartyScriptApi<GoogleTagManagerApi>  {
+  const gtm = GoogleTagManager({ id: options.id })
+  validateRequiredOptions(gtm.id, options, ['id'])
 
-  useServerHead({
-    script: [
-      {
-        key: `${key}:init`,
-        innerHTML: 'window.dataLayer=window.dataLayer||[];window.dataLayer.push({\'gtm.start\':new Date().getTime(),event:\'gtm.js\'});',
-      },
-    ],
-    noscript: [
-      options.id
-        ? {
-            key: `${key}:fallback`,
-            innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${options.id}"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }
-        : {},
-    ],
-  })
-  // TODO reactivity
-  return useScript<GoogleTagManagerApi>({
-    key,
-    src: withQuery('https://www.googletagmanager.com/gtm.js', { id: options.id }),
-  }, {
-    ...options,
+  return convertThirdPartyCapital<GoogleTagManagerOptions, GoogleTagManagerApi>({
+    data: gtm,
+    mainScriptKey: 'gtm',
+    options,
     use: () => ({ dataLayer: window.dataLayer, google_tag_manager: window.google_tag_manager }),
   })
 }
