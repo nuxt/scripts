c693cdf7b17aa853b18dce2e1d51513da4dd56d4
Daniel Roe <daniel@roe.dev>
2024-03-07 09:22:18 +0000

    chore: refactor directories

--- modules/nuxt-assets/src/runtime/composables/useProxyAsset.ts
+++
@@ -1,9 +0,0 @@
-import { withQuery } from 'ufo'
-import { useRuntimeConfig } from 'nuxt/app'
-
-export function useProxyAsset(url: string) {
-  const { routePrefix } = useRuntimeConfig().public['nuxt-assets']
-  return withQuery(`${routePrefix}/proxy`, {
-    url: encodeURIComponent(url),
-  })
-}
