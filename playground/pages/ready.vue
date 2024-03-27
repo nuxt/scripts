948b2c53dfb0bfb2e68d584fd58ee8fca679ff45
harlan <harlan@harlanzw.com>
2024-03-28 02:26:54 +1100

    chore: lint

---
+++ playground/pages/ready.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { useScript } from '#imports'
+
+const { $script } = useScript('https://www.googletagmanager.com/gtag/js?id=GTM-5ZQZJZ', {
+  trigger: 'onNuxtReady',
+  assetStrategy: 'bundle',
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      {{ $script.status }}
+    </div>
+  </div>
+</template>
