116217ed151e09488b5e4d6d779fe21a8dd7170b
harlan <harlan@harlanzw.com>
2024-04-22 11:10:52 +1000

    fix(useElementScriptTrigger): prefer Promise returns

--- src/runtime/composables/useElementScriptTrigger.ts
+++ src/runtime/composables/useElementScriptTrigger.ts
@@ -4,10 +4,10 @@ import { ref } from 'vue'
 
 export type ElementScriptTrigger = 'visible' | 'mouseover' | false
 
-export function useElementScriptTrigger(trigger: ElementScriptTrigger | undefined, root?: HTMLElement | Ref<HTMLElement | undefined> | null) {
-  if (import.meta.server || !root)
-    return 'client'
+export function useElementScriptTrigger(trigger: ElementScriptTrigger | undefined, el?: HTMLElement | Ref<HTMLElement | undefined> | null) {
+  if (import.meta.server || !el)
+    return new Promise<void>(() => {})
 
-  const activeRef = trigger ? (trigger === 'mouseover' ? useElementHover(root) : useElementVisibility(root)) : ref(false)
-  return trigger ? new Promise<void>(resolve => watchOnce(activeRef, resolve)) : 'client'
+  const activeRef = trigger ? (trigger === 'mouseover' ? useElementHover(el) : useElementVisibility(el)) : ref(false)
+  return trigger ? new Promise<void>(resolve => watchOnce(activeRef, resolve)) : Promise.resolve()
 }
