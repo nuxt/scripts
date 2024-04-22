53a16db6cee3e1174c2bf63d7f960e2ecc514813
Harlan Wilton <harlan@harlanzw.com>
2024-04-22 22:57:04 +1000

    feat: plausible analytics (#26)

---
+++ playground/pages/third-parties/plausible-analytics.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { useScriptPlausibleAnalytics } from '../../../src/runtime/registry/plausible-analytics'
+import { ref, useHead } from '#imports'
+
+useHead({
+  title: 'Plausible',
+})
+
+// composables return the underlying api as a proxy object and a $script with the script state
+const { $script, plausible } = useScriptPlausibleAnalytics({
+  domain: 'scripts.nuxt.com',
+  extension: 'local',
+})
+// this will be triggered once the script is ready async
+plausible('404', { props: { path: '/404' } })
+
+const clicks = ref(0)
+async function clickHandler() {
+  clicks.value++
+  plausible('test', { props: { clicks: clicks.value } })
+}
+</script>
+
+<template>
+  <div>
+    <UButton @click="clickHandler">
+      important conversion {{ clicks }}
+    </UButton>
+    <ClientOnly>
+      <div>
+        status: {{ $script.status.value }}
+      </div>
+    </ClientOnly>
+  </div>
+</template>
