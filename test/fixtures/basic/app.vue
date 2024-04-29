380a814a390fbaa3d6d78d5e8fffdeb57b9259c1
harlan <harlan@harlanzw.com>
2024-04-29 23:22:55 +1000

    tests: basic e2e script load test

---
+++ test/fixtures/basic/app.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { onNuxtReady, useScript } from '#imports'
+
+const { myScript } = useScript<{ myScript: (arg: string) => void }>('/myScript.js', {
+  trigger: onNuxtReady,
+  use() {
+    return {
+      // @ts-expect-error untyped
+      myScript: window.myScript,
+    }
+  },
+})
+
+myScript('test')
+</script>
+
+<template>
+  <div>hello world</div>
+</template>
