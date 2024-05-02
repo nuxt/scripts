8ccbb1a2fe2ef323c98a4cba5adffe53dad82062
Julien Huang <julien.h.dev@gmail.com>
2024-05-02 09:35:37 +0200

    fix(segments): fix segments api

--- src/runtime/registry/segment.ts
+++ src/runtime/registry/segment.ts
@@ -10,24 +10,38 @@ export const SegmentOptions = object({
 
 export type SegmentInput = RegistryScriptInput<typeof SegmentOptions>
 
+interface AnalyticsApi {
+  track: (event: string, properties?: Record<string, any>) => void
+  page: (name?: string, properties?: Record<string, any>) => void
+  identify: (userId: string, traits?: Record<string, any>, options?: Record<string, any>) => void
+  group: (groupId: string, traits?: Record<string, any>, options?: Record<string, any>) => void
+  alias: (userId: string, previousId: string, options?: Record<string, any>) => void
+  reset: () => void
+  /**
+   * @internal
+   */
+  methods: string[]
+  /**
+   * @internal
+   */
+  factory: (method: string) => (...args: any[]) => AnalyticsApi
+  /**
+   * @internal
+   */
+  push: (args: any[]) => void
+}
+
 export interface SegmentApi {
-  analytics: {
-    track: (event: string, properties?: Record<string, any>) => void
-    page: (name?: string, properties?: Record<string, any>) => void
-    identify: (userId: string, traits?: Record<string, any>, options?: Record<string, any>) => void
-    group: (groupId: string, traits?: Record<string, any>, options?: Record<string, any>) => void
-    alias: (userId: string, previousId: string, options?: Record<string, any>) => void
-    reset: () => void
-  }
+  analytics: AnalyticsApi & { [key: string]: (...args: any[]) => AnalyticsApi }
 }
 
 declare global {
-  interface Window extends SegmentApi {}
+  interface Window extends SegmentApi { }
 }
 
 export function useScriptSegment<T extends SegmentApi>(_options?: SegmentInput) {
   return registryScript<T, typeof SegmentOptions>('segment', (options) => {
-    const analyticsKey = options?.analyticsKey || 'analytics'
+    const analyticsKey: string = options?.analyticsKey ?? 'analytics'
     return {
       scriptInput: {
         'data-global-segment-analytics-key': analyticsKey,
@@ -36,27 +50,28 @@ export function useScriptSegment<T extends SegmentApi>(_options?: SegmentInput) 
       schema: import.meta.dev ? SegmentOptions : undefined,
       scriptOptions: {
         use() {
-          return { analytics: window[analyticsKey] }
+          // @ts-expect-error
+          return { analytics: window[analyticsKey] as SegmentApi['analytics'] }
         },
         clientInit: import.meta.server
           ? undefined
           : () => {
-              window.analytics = window.analytics || []
-              window.analytics.methods = ['track', 'page', 'identify', 'group', 'alias', 'reset']
-              window.analytics.factory = function (method) {
-                return function (...params) {
-                  const args = Array.prototype.slice.call(params)
-                  args.unshift(method)
-                  window.analytics.push(args)
-                  return window.analytics
-                }
+            window.analytics = window.analytics || []
+            window.analytics.methods = ['track', 'page', 'identify', 'group', 'alias', 'reset']
+            window.analytics.factory = function (method) {
+              return function (...params) {
+                const args = Array.prototype.slice.call(params)
+                args.unshift(method)
+                window.analytics.push(args)
+                return window.analytics
               }
-              for (let i = 0; i < window.analytics.methods.length; i++) {
-                const key = window.analytics.methods[i]
-                window.analytics[key] = window.analytics.factory(key)
-              }
-              window.analytics.page()
-            },
+            }
+            for (let i = 0; i < window.analytics.methods.length; i++) {
+              const key = window.analytics.methods[i]
+              window.analytics[key] = window.analytics.factory(key)
+            }
+            window.analytics.page()
+          },
       },
     }
   }, _options)
