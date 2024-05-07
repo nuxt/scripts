73d3bb29c187f1ca5ce3dc6c02b440dc89101d45
harlan <harlan@harlanzw.com>
2024-05-07 11:56:52 +1000

    chore: cloudflare web analytics comparison

---
+++ playground/pages/third-parties/cloudflare-web-analytics/default.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useHead } from '#imports'
+
+useHead({
+  script: [
+    {
+      'src': 'https://static.cloudflareinsights.com/beacon.min.js',
+      'defer': true,
+      'data-cf-beacon': JSON.stringify({
+        token: 'ade278253a19413c9bd923b079870902',
+      }),
+    },
+  ],
+})
+</script>
+
+<template>
+<div>useHead</div>
+</template>
