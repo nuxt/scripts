91dea25049c0d80cee4d6b208e9cac232147b949
harlan <harlan@harlanzw.com>
2024-06-04 13:23:57 +1000

    chore: tpc test

--- playground/composables/useScriptGoogleAnalyticsTpc.ts
+++ playground/composables/useScriptGoogleAnalyticsTpc.ts
@@ -1,40 +1,42 @@
-import {useRegistryScript} from "#nuxt-scripts-utils";
-import {GoogleAnalyticsScriptResolver} from "../../src/registry";
-import type {GoogleAnalyticsInput} from "../../src/runtime/registry/google-analytics";
-import {GoogleAnalyticsOptions} from "../../src/runtime/registry/google-analytics";
+import type { GTag, GoogleAnalyticsApi } from 'third-party-capital'
 import { GoogleAnalytics } from 'third-party-capital'
+import type { GoogleAnalyticsInput } from '../../src/runtime/registry/google-analytics'
+import { GoogleAnalyticsOptions } from '../../src/runtime/registry/google-analytics'
+import { useRegistryScript } from '#nuxt-scripts-utils'
 
 export function useScriptGoogleAnalyticsTpc<T extends GoogleAnalyticsApi>(_options?: GoogleAnalyticsInput) {
-  const val = GoogleAnalytics(_options)
   // Note: inputs.useScriptInput is not usable, needs to be normalized
-  return useRegistryScript<T, typeof GoogleAnalyticsOptions>('googleAnalytics', options => ({
-    scriptInput: {
-      src: GoogleAnalyticsScriptResolver(options),
-    },
-    schema: import.meta.dev ? GoogleAnalyticsOptions : undefined,
-    scriptOptions: {
-      use() {
-        return { dataLayer: window.dataLayer, gtag: window.gtag }
+  return useRegistryScript<T, typeof GoogleAnalyticsOptions>('googleAnalytics', (options) => {
+    const output = GoogleAnalytics(_options!)
+    return {
+      scriptInput: {
+        src: output.scripts![0]!.url,
       },
-      // allow dataLayer to be accessed on the server
-      stub: import.meta.client
-        ? undefined
-        : ({ fn }) => {
-          return fn === 'dataLayer' ? [] : undefined
+      schema: import.meta.dev ? GoogleAnalyticsOptions : undefined,
+      scriptOptions: {
+        use() {
+          return { dataLayer: window.dataLayer, gtag: window.gtag }
         },
-    },
-    clientInit: import.meta.server
-      ? undefined
-      : () => {
-        const w = window
-        w.dataLayer = w.dataLayer || []
-        const gtag: GTag = function () {
-          // eslint-disable-next-line prefer-rest-params
-          w.dataLayer.push(arguments)
-        }
-        gtag('js', new Date())
-        gtag('config', options?.id)
-        w.gtag = gtag
+        // allow dataLayer to be accessed on the server
+        stub: import.meta.client
+          ? undefined
+          : ({ fn }) => {
+              return fn === 'dataLayer' ? [] : undefined
+            },
       },
-  }), _options)
+      clientInit: import.meta.server
+        ? undefined
+        : () => {
+            const w = window
+            w.dataLayer = w.dataLayer || []
+            const gtag: GTag = function () {
+            // eslint-disable-next-line prefer-rest-params
+              w.dataLayer.push(arguments)
+            }
+            gtag('js', new Date())
+            gtag('config', options?.id)
+            w.gtag = gtag
+          },
+    }
+  }, _options)
 }
--- playground/pages/third-parties/google-analytics/tpc.vue
+++ playground/pages/third-parties/google-analytics/tpc.vue
@@ -1,46 +1,10 @@
 <script lang="ts" setup>
-import {type GoogleAnalyticsInput, GoogleAnalyticsOptions, useHead, useScriptGoogleAnalytics} from '#imports'
-import {useRegistryScript} from "#nuxt-scripts-utils";
-import {GoogleAnalyticsScriptResolver} from "../../../../src/registry";
+import { useHead } from '#imports'
+import { useScriptGoogleAnalyticsTpc } from '~/composables/useScriptGoogleAnalyticsTpc'
 
 useHead({
-  title: 'Google Analytics',
+  title: 'Google Analytics TPC',
 })
-
-
-export function useScriptGoogleAnalytics<T extends GoogleAnalyticsApi>(_options?: GoogleAnalyticsInput) {
-  // Note: inputs.useScriptInput is not usable, needs to be normalized
-  return useRegistryScript<T, typeof GoogleAnalyticsOptions>('googleAnalytics', options => ({
-    scriptInput: {
-      src: GoogleAnalyticsScriptResolver(options),
-    },
-    schema: import.meta.dev ? GoogleAnalyticsOptions : undefined,
-    scriptOptions: {
-      use() {
-        return { dataLayer: window.dataLayer, gtag: window.gtag }
-      },
-      // allow dataLayer to be accessed on the server
-      stub: import.meta.client
-        ? undefined
-        : ({ fn }) => {
-          return fn === 'dataLayer' ? [] : undefined
-        },
-    },
-    clientInit: import.meta.server
-      ? undefined
-      : () => {
-        const w = window
-        w.dataLayer = w.dataLayer || []
-        const gtag: GTag = function () {
-          // eslint-disable-next-line prefer-rest-params
-          w.dataLayer.push(arguments)
-        }
-        gtag('js', new Date())
-        gtag('config', options?.id)
-        w.gtag = gtag
-      },
-  }), _options)
-}
 
 // composables return the underlying api as a proxy object and a $script with the script state
 const { gtag, $script } = useScriptGoogleAnalyticsTpc({
