48587911ab8ca49d161807abf337e4e82f3a54f0
Julien Huang <julien.h.dev@gmail.com>
2024-06-26 15:11:59 +0200

    fix: double encoded

--- src/runtime/composables/useFeatureDetection.ts
+++ src/runtime/composables/useFeatureDetection.ts
@@ -2,12 +2,10 @@ import { onMounted } from 'vue'
 
 export function useFeatureDetection(id: string) {
   onMounted(() => {
-    onMounted(() => {
-      performance?.mark?.('mark_feature_usage', {
-        detail: {
-          feature: `nuxt-third-parties-${id}`,
-        },
-      })
+    performance?.mark?.('mark_feature_usage', {
+      detail: {
+        feature: `nuxt-third-parties-${id}`,
+      },
     })
   })
 }
