8af24d66569697b132f00efa7e6a3ded63776256
Julien Huang <julien.h.dev@gmail.com>
2024-06-26 13:22:04 +0200

    chore: lint

--- test/types/types.test-d.ts
+++ test/types/types.test-d.ts
@@ -1,9 +1,9 @@
-import { describe, expectTypeOf, it } from "vitest"
-import type { ModuleOptions } from "../../dist/module"
-import type { ScriptRegistry } from "../../src/runtime/types"
+import { describe, expectTypeOf, it } from 'vitest'
+import type { ModuleOptions } from '../../dist/module'
+import type { ScriptRegistry } from '../../src/runtime/types'
 
 describe('module options registry', async () => {
-    it('expect no any', async ()  => {
-      expectTypeOf<NonNullable<NonNullable<ModuleOptions>['registry']>[keyof ScriptRegistry]>().not.toBeAny()
-    })
+  it('expect no any', async () => {
+    expectTypeOf<NonNullable<NonNullable<ModuleOptions>['registry']>[keyof ScriptRegistry]>().not.toBeAny()
+  })
 })
