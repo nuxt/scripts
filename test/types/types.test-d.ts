9a9812bdba4e64df529460399bf17864d20b82b6
Julien Huang <julien.h.dev@gmail.com>
2024-06-26 12:45:35 +0200

    test: type registry type

---
+++ test/types/types.test-d.ts
@@ -0,0 +1,9 @@
+import { describe, expectTypeOf, it } from "vitest"
+import type { ModuleOptions } from "../../dist/module"
+import type { ScriptRegistry } from "../../src/runtime/types"
+
+describe('module options registry', async () => {
+    it('expect no any', async ()  => {
+      expectTypeOf<NonNullable<NonNullable<ModuleOptions>['registry']>[keyof ScriptRegistry]>().not.toBeAny()
+    })
+})
