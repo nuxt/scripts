d8b79f1f2c8334cc04afb11829d2f5b60f3be03c
harlan <harlan@harlanzw.com>
2024-09-03 01:40:49 +1000

    chore: throw error if nuxt api is down
    Fixes #229

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -17,7 +17,6 @@ export default defineNuxtConfig({
     async (_, nuxt) => {
       // build time for caching
       const { contributors } = await $fetch(`https://api.nuxt.com/modules/scripts`)
-        .catch(() => ({ contributors: [] }))
       nuxt.options.runtimeConfig.public.contributors = contributors.map(m => m.id)
     },
   ],
