cc1f5d079dc63f9527144cfd9e718c6c7ebef95e
harlan <harlan@harlanzw.com>
2024-10-15 18:06:26 +1100

    chore: type issues

--- types.d.ts
+++ types.d.ts
@@ -3,6 +3,7 @@
 declare module '#app' {
   interface NuxtApp {
     _scripts: Record<string, (import('#nuxt-scripts').NuxtDevToolsScriptInstance)>
+    $scripts: Record<string, any>
   }
   interface RuntimeNuxtHooks {
     'scripts:updated': (ctx: { scripts: Record<string, (import('#nuxt-scripts').NuxtDevToolsScriptInstance)> }) => void | Promise<void>
