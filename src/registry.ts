ebcb3281e63340bc9b0b79c042f1a2e6bd541c7e
Mark1 <3292578+0ahz@users.noreply.github.com>
2025-02-07 14:12:15 +0800

    feat: add umami analytics to registry (#348)

--- src/registry.ts
+++ src/registry.ts
@@ -252,5 +252,14 @@ export const registry: (resolve?: (s: string) => string) => RegistryScripts = (r
         return withQuery('https://www.googletagmanager.com/gtag/js', { id: options?.id, l: options?.l })
       },
     },
+    {
+      label: 'Umami Analytics',
+      category: 'analytics',
+      logo: `<svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24"><path fill="currentColor" d="M2.203 8.611H.857a.845.845 0 0 0-.841.841v.858a13 13 0 0 0-.016.6c0 6.627 5.373 12 12 12c6.527 0 11.837-5.212 11.996-11.701c0-.025.004-.05.004-.075V9.452a.845.845 0 0 0-.841-.841h-1.346c-1.159-4.329-5.112-7.521-9.805-7.521S3.363 4.282 2.203 8.611m18.444 0H3.37c1.127-3.702 4.57-6.399 8.638-6.399c4.069 0 7.512 2.697 8.639 6.399"/></svg>`,
+      import: {
+        name: 'useScriptUmamiAnalytics',
+        from: resolve('./runtime/registry/umami-analytics'),
+      },
+    },
   ]
 }
