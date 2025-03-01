199164da0e53b2119f894464c790281d8b622289
harlan <harlan@harlanzw.com>
2025-03-01 23:18:35 +1100

    chore: remove deprecated api usage

--- test/fixtures/basic/pages/bundle-use-script.vue
+++ test/fixtures/basic/pages/bundle-use-script.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { injectHead, ref, useScript } from '#imports'
 
-const { proxy, onLoaded } = useScript<{ myScript: (arg: string) => void }>(
+const { onLoaded } = useScript<{ myScript: (arg: string) => void }>(
   // need a real script to bundle
   'https://code.jquery.com/jquery-3.6.0.min.js',
 {
@@ -21,8 +21,6 @@ onLoaded(async () => {
   const tags = await head.resolveTags()
   scriptSrc.value = tags.filter(s => s.tag === 'script')[0].props.src
 })
-
-proxy.myScript('test')
 </script>
 
 <template>
