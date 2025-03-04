b494283034f37a1a3b633a98313469438dfd46f9
harlan <harlan@harlanzw.com>
2025-03-05 01:54:30 +1100

    chore: vitest `@nuxt/scripts` resolution

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,12 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     globals: true,
+    environmentOptions: {
+      nuxt: {
+        overrides: {
+          modules: ['@nuxt/scripts'],
+        },
+      },
+    },
   },
 })
