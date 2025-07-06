35f9f869c854a9a722a524dbad58133e4593a8c3
Harlan Wilton <harlan@harlanzw.com>
2025-07-06 12:55:34 +1000

    fix: support cdnURL for bundled scripts
    Fixes #464

---
+++ test/fixtures/cdn/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>CDN URL Test</h1>
+    <button @click="loadScript">
+      Load Script
+    </button>
+    <div id="script-status">
+      {{ scriptStatus }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const scriptStatus = ref('ready')
+
+function loadScript() {
+  const { $script } = useScript('https://cdn.jsdelivr.net/npm/confetti-js@0.0.18/dist/index.min.js', {
+    bundle: true,
+    use() {
+      return {
+        ConfettiGenerator: window.ConfettiGenerator,
+      }
+    },
+  })
+
+  $script.then((script) => {
+    scriptStatus.value = 'loaded'
+    // Script loaded successfully
+  }).catch(() => {
+    scriptStatus.value = 'error'
+  })
+}
+</script>
