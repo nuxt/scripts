7043fd34045c9fc36b78f63f219ed7c216510b67
Harlan Wilton <harlan@harlanzw.com>
2025-12-20 16:41:27 +1100

    feat(playground): add PostHog demo page

---
+++ playground/pages/third-parties/posthog/nuxt-scripts.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { useHead, useScriptPostHog } from '#imports'
+
+useHead({
+  title: 'PostHog',
+})
+
+const { status, proxy, onLoaded } = useScriptPostHog({
+  apiKey: 'phc_YOUR_API_KEY',
+})
+
+function captureEvent() {
+  proxy.posthog.capture('button_clicked', {
+    button_name: 'test',
+  })
+}
+
+function identifyUser() {
+  proxy.posthog.identify('user@example.com', {
+    email: 'user@example.com',
+    name: 'Test User',
+  })
+}
+
+onLoaded(({ posthog }) => {
+  console.log('PostHog loaded:', posthog)
+})
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <div>
+        status: {{ status }}
+      </div>
+      <div class="flex gap-2 mt-4">
+        <UButton @click="captureEvent">
+          Capture Event
+        </UButton>
+        <UButton @click="identifyUser">
+          Identify User
+        </UButton>
+      </div>
+    </ClientOnly>
+  </div>
+</template>
