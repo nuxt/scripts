abb95a27df0fa1878e793d4fef6e2e24c6b7b215
Harlan Wilton <harlan@harlanzw.com>
2026-02-25 01:34:11 +1100

    perf: migrate plugins to oxc-walker (parseAndWalk) (#610)

--- src/plugins/check-scripts.ts
+++ src/plugins/check-scripts.ts
@@ -1,7 +1,6 @@
 import { createUnplugin } from 'unplugin'
-import { walk } from 'estree-walker'
-import type { Node } from 'estree-walker'
-import type { AssignmentExpression, CallExpression, ObjectPattern, ArrowFunctionExpression, Identifier, MemberExpression } from 'estree'
+import { parseAndWalk } from 'oxc-walker'
+import type { Node } from 'oxc-parser'
 import { isVue } from './util'
 
 export function NuxtScriptsCheckScripts() {
@@ -12,50 +11,46 @@ export function NuxtScriptsCheckScripts() {
         filter: {
           id: /\.vue/,
         },
-        async handler(code, id) {
+        handler(code, id) {
           if (!isVue(id, { type: ['script'] }))
             return
           if (!code.includes('useScript')) // all integrations should start with useScript*
             return
 
-          const ast = this.parse(code)
           let nameNode: Node | undefined
           let errorNode: Node | undefined
-          walk(ast as Node, {
-            enter(_node) {
-              if (_node.type === 'VariableDeclaration' && _node.declarations?.[0]?.id?.type === 'ObjectPattern') {
-                const objPattern = _node.declarations[0]?.id as ObjectPattern
-                for (const property of objPattern.properties) {
-                  if (property.type === 'Property' && property.key.type === 'Identifier' && property.key.name === '$script' && property.value.type === 'Identifier') {
-                    nameNode = _node
+          parseAndWalk(code, id, function (_node) {
+            if (_node.type === 'VariableDeclaration' && (_node as any).declarations?.[0]?.id?.type === 'ObjectPattern') {
+              const objPattern = (_node as any).declarations[0]?.id
+              for (const property of objPattern.properties) {
+                if (property.type === 'Property' && property.key.type === 'Identifier' && property.key.name === '$script' && property.value.type === 'Identifier') {
+                  nameNode = _node
+                }
+              }
+            }
+            if (nameNode) {
+              let sequence: any = _node.type === 'SequenceExpression' ? _node : null
+              let assignmentExpression: any
+              if (_node.type === 'VariableDeclaration') {
+                if ((_node as any).declarations[0]?.init?.type === 'SequenceExpression') {
+                  sequence = (_node as any).declarations[0]?.init
+                  assignmentExpression = (_node as any).declarations[0]?.init?.expressions?.[0]
+                }
+              }
+              if (sequence && !assignmentExpression) {
+                assignmentExpression = (sequence.expressions[0]?.type === 'AssignmentExpression' ? sequence.expressions[0] : null)
+              }
+              if (assignmentExpression) {
+              // check right call expression is calling $script
+                const right = assignmentExpression?.right
+                if (right.callee?.name === '_withAsyncContext') {
+                  if (right.arguments[0]?.body?.name === '$script'
+                    || right.arguments[0]?.body?.callee?.object?.name === '$script') {
+                    errorNode = nameNode
                   }
                 }
               }
-              if (nameNode) {
-                let sequence = _node.type === 'SequenceExpression' ? _node : null
-                let assignmentExpression
-                if (_node.type === 'VariableDeclaration') {
-                  if (_node.declarations[0]?.init?.type === 'SequenceExpression') {
-                    sequence = _node.declarations[0]?.init
-                    assignmentExpression = _node.declarations[0]?.init?.expressions?.[0]
-                  }
-                }
-                if (sequence && !assignmentExpression) {
-                  assignmentExpression = (sequence.expressions[0]?.type === 'AssignmentExpression' ? sequence.expressions[0] : null)
-                }
-                if (assignmentExpression) {
-                // check right call expression is calling $script
-                  const right = (assignmentExpression as AssignmentExpression)?.right as CallExpression
-                  // @ts-expect-error untyped
-                  if (right.callee?.name === '_withAsyncContext') {
-                    if (((right.arguments[0] as ArrowFunctionExpression)?.body as Identifier)?.name === '$script'
-                      || ((((right.arguments[0] as ArrowFunctionExpression)?.body as CallExpression)?.callee as MemberExpression)?.object as Identifier)?.name === '$script') {
-                      errorNode = nameNode
-                    }
-                  }
-                }
-              }
-            },
+            }
           })
           if (errorNode) {
             return this.error(new Error('You can\'t use a top-level await on $script as it will never resolve.'))
